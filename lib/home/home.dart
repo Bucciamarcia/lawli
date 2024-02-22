bb35dd53fec60466d0cba57899a07f5ed7454399
Stefano Mini <info@stefanominiconsulting.com>
2024-02-22 15:29:24 +0200

    Redirect a login

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:lawli/home/login/login.dart';
+import 'package:lawli/home/recent/recent.dart';
+
+import '../services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text('Welcome to Lawli'),
-      ),
-    );
+    return StreamBuilder(
+        stream: AuthService().userStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return const Text("ERROR!");
+          } else if (snapshot.hasData) {
+            return const RecentScreen();
+          } else {
+            return const LoginScreen();
+          }
+        });
   }
 }
