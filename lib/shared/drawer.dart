2bffdc1862d1f85a003bfbcd4f2bcddeb3e28987
Stefano Mini <info@stefanominiconsulting.com>
2024-02-24 07:35:53 +0200

    Refactor menu drawer

--- lib/shared/drawer.dart
+++ lib/shared/drawer.dart
@@ -1,43 +1,45 @@
 import 'package:flutter/material.dart';
-import "../services/auth.dart";
+import "../services/services.dart";
 
 class ResponsiveDrawer extends StatelessWidget {
   const ResponsiveDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    if (AuthService().isSignedIn()) {
-      return Drawer(
-        backgroundColor:
-            MediaQuery.of(context).size.width > 600 ? Colors.grey[300] : null,
-        shape: const BeveledRectangleBorder(
-          borderRadius: BorderRadius.zero,
-        ),
-        child: ListView(
-          children: MenuElements().listElements,
-        ),
-      );
-    } else {
-      return Drawer(
-        child: ListView(
-          children: const [
-            ListTile(title: Text('Log in')),
-          ],
-        ),
-      );
-    }
+    return Drawer(
+      backgroundColor:
+          ResponsiveLayout.isDesktop(context) ? Colors.grey[300] : null,
+      shape: const BeveledRectangleBorder(
+        borderRadius: BorderRadius.zero,
+      ),
+      child: ListView(
+        children:
+            AuthService().isSignedIn() ? MenuElements(context: context).loggedInElements() : MenuElements(context: context).loggedOutElements(),
+      ),
+    );
   }
 }
 
 class MenuElements {
-  List<Widget> listElements = [
-    const SizedBox(
-        height: 100, child: DrawerHeader(child: Text('Drawer Header'))),
-    const ListTile(title: Text('Item 1')),
-    const ListTile(title: Text('Item 2')),
-  ];
-}
+  final BuildContext context;
+  List<Widget> listElements = [];
 
-class DrawerStyle {
-  
+  MenuElements({required this.context});
+
+  loggedInElements() {
+    listElements = [
+      SizedBox(
+          height: ResponsiveLayout.isDesktop(context) ? 100 : null,
+          child: const DrawerHeader(child: Text('Drawer Header'))),
+      const ListTile(title: Text('Item 1')),
+      const ListTile(title: Text('Item 2')),
+    ];
+    return listElements;
+  }
+
+  loggedOutElements() {
+    listElements = [
+      const ListTile(title: Text('Log in')),
+    ];
+  }
 }
