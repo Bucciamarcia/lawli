a488c1f1d03398d16b8c90143c83c0a627fd91bc
Stefano Mini <info@stefanominiconsulting.com>
2024-02-25 17:38:47 +0200

    Rimuovi assistito finito

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -11,7 +11,8 @@ class FirestoreService {
 
   Future<Object?> getUserData(String user) async {
     try {
-      final DocumentSnapshot userDoc = await _db.collection('users').doc(user).get();
+      final DocumentSnapshot userDoc =
+          await _db.collection('users').doc(user).get();
       if (userDoc.data() == null) {
         log("User data null");
       } else {
@@ -26,10 +27,7 @@ class FirestoreService {
 
   Future<void> addUserToDb(final String id, final bool anon) async {
     try {
-      FirebaseFirestore.instance
-          .collection("users")
-          .doc(id)
-          .set(
+      FirebaseFirestore.instance.collection("users").doc(id).set(
         {
           "account": false,
         },
@@ -40,7 +38,8 @@ class FirestoreService {
     }
   }
 
-  Future<DocumentReference<Map<String, dynamic>>> retrieveAccountObject() async {
+  Future<DocumentReference<Map<String, dynamic>>>
+      retrieveAccountObject() async {
     try {
       final userDocument = await _db.collection("users").doc(userId).get();
       final accountName = userDocument.get('account');
@@ -50,36 +49,35 @@ class FirestoreService {
       rethrow;
     }
   }
-
 }
 
 class AccountDb extends FirestoreService {
-
   Future<String> getAccountName() async {
     try {
-      final DocumentSnapshot userDoc = await _db.collection('users').doc(userId).get();
+      final DocumentSnapshot userDoc =
+          await _db.collection('users').doc(userId).get();
       return userDoc.get('account');
     } catch (e) {
       log('Error getting account name: $e');
       return "Error";
     }
-  
   }
-
-  
 }
 
 class AssistitoDb extends FirestoreService {
-
   Future<void> deleteAssistito(final double assistitoId) async {
+    await removeFromListaAssistiti(assistitoId);
     try {
       final accountRef = await retrieveAccountObject();
-      await accountRef.collection("assistiti").doc(assistitoId.toString()).delete();
-      debugPrint("Deleted assistito $assistitoId");
-      await decreaseCounter();
+      await accountRef
+          .collection("assistiti")
+          .doc(assistitoId.toString())
+          .delete();
     } catch (e) {
       debugPrint("Error deleting assistito: $e");
+      rethrow;
     }
+    await decreaseCounter();
   }
 
   Future<void> decreaseCounter() async {
@@ -94,20 +92,51 @@ class AssistitoDb extends FirestoreService {
       debugPrint("Decreased assistiti counter");
     } catch (e) {
       debugPrint("Error decreasing assistiti counter: $e");
+      rethrow;
     }
+  }
+
+  Future<void> removeFromListaAssistiti(assistitoId) async {
+    DocumentReference docRef = await retrieveAccountObject();
+    debugPrint("DOCREF: $docRef");
+    List<Assistito> assistiti = await RetrieveObjectFromDb().getAssistiti();
+    debugPrint("ASSISTITI: $assistiti");
+    debugPrint("PRIMO ASSISTITO ID: ${assistiti[0].id}");
+    debugPrint("ASSISTITO ID: $assistitoId");
+    Assistito assistito = assistiti.firstWhere((element) => element.id == assistitoId);
+    debugPrint("ASSISTITO: $assistito");
+
+    String firstname = assistito.nome;
+    debugPrint("FIRSTNAME: $firstname");
+    String lastname = assistito.cognome;
+    String fullName = "$firstname $lastname";
+
+    try {
+  DocumentSnapshot docSnapshot = await docRef.get();
+  List listToUpdate = docSnapshot.get("lista_assistiti");
+  debugPrint("LISTA: $listToUpdate");
+  listToUpdate.remove(fullName);
+  await docRef.update({"lista_assistiti": listToUpdate});
+  debugPrint("Removed assistito from lista_assistiti");
+} on Exception catch (e) {
+  debugPrint("Error removing assistito from lista_assistiti: $e");
+  rethrow;
+}
+
+
+
   }
 }
 
 class RetrieveObjectFromDb extends FirestoreService {
-
   Future<List<Assistito>> getAssistiti() async {
     String accountName = await AccountDb().getAccountName();
 
-    var ref = _db.collection("accounts").doc(accountName).collection("assistiti");
+    var ref =
+        _db.collection("accounts").doc(accountName).collection("assistiti");
     var snapshot = await ref.get();
     var data = snapshot.docs.map((s) => s.data());
     var topics = data.map((d) => Assistito.fromJson(d));
     return topics.toList();
   }
-  
 }
