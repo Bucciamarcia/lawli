dea427a61796f8b8a104588ba094e5b420ffa4aa
Stefano Mini <info@stefanominiconsulting.com>
2024-02-29 09:34:49 +0200

    kdebug in provider

--- lib/services/provider.dart
+++ lib/services/provider.dart
@@ -1,7 +1,9 @@
 import "package:flutter/material.dart";
+import 'package:flutter/foundation.dart' show kDebugMode;
 
 class DashboardProvider extends ChangeNotifier {
-  double idPratica = 0;
+
+  double idPratica = kDebugMode ? 1 : 0;
 
   void setIdPratica(double id) {
     idPratica = id;
