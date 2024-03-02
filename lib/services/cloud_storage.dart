b55e8abddddef36c37f2de8a648f874a477fec68
Stefano Mini <info@stefanominiconsulting.com>
2024-03-02 12:38:45 +0200

    content type upload

--- lib/services/cloud_storage.dart
+++ lib/services/cloud_storage.dart
@@ -2,21 +2,34 @@ import 'dart:typed_data';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:lawli/services/firestore.dart';
+import "package:path/path.dart" as p;
 
 class StorageService {
   final FirebaseStorage storage = FirebaseStorage.instance;
   final Reference storageRef = FirebaseStorage.instance.ref();
 
-  Future<void> uploadNewDocument (String idPratica, String fileName, Uint8List file) async {
-
+  Future<void> uploadNewDocument(
+      String idPratica, String fileName, Uint8List file) async {
     final String accountName = await AccountDb().getAccountName();
+    final String extension = p.extension(fileName);
+    late String contentType;
+    if (extension == ".txt") {
+      contentType = "text/plain";
+    } else if (extension == ".pdf") {
+      contentType = "application/pdf";
+    } else if (extension == ".docx") {
+      contentType =
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+    } else {
+      throw Exception("File type not supported");
+    }
 
     try {
-      var docRef = storageRef.child("accounts/$accountName/pratiche/$idPratica/documenti/originale_$fileName");
-      await docRef.putData(file, SettableMetadata(contentType: "text/plain"));
+      var docRef = storageRef.child(
+          "accounts/$accountName/pratiche/$idPratica/documenti/originale_$fileName");
+      await docRef.putData(file, SettableMetadata(contentType: contentType));
     } catch (e) {
       debugPrint("Error uploading file: $e");
     }
   }
-
 }
