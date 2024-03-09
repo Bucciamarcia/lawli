d36c359e3ef884f22ec3c782c5d74f6bd9ef4311
Stefano Mini <info@stefanominiconsulting.com>
2024-03-09 15:22:34 +0200

    Add file extension check in Generated_Document class

--- functions/py/functions/generate_document_summary.py
+++ functions/py/functions/generate_document_summary.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import os
 
 class Generated_Document:
     def __init__(self, logger:logging.Logger, payload:str, object_id:str):
@@ -10,8 +11,21 @@ class Generated_Document:
         except Exception as e:
             self.logger.error(f"Error loading json: {e}")
             raise e
+        self.filename = os.path.basename(self.object_id)
+        
+    def check_ext(self):
+        """Check if the file is a txt file."""
+        if self.filename.endswith(".txt"):
+            return True
+        else:
+            return False
         
     def process_document(self) -> str:
         """Process the document."""
-        pass
-        return "ok"
+        
+        if self.check_ext():
+            self.logger.info(f"File {self.object_id} is a txt file. Processing...")
+            return
+        else:
+            self.logger.info(f"File {self.object_id} is not a txt file - exiting.")
+            return
