48f85a2c686b88fa8906ab5deb029064998e6abe
Stefano Mini <info@stefanominiconsulting.com>
2024-03-15 20:59:52 +0200

    Remove unused import statement in LoginBlock

--- lib/home/login/loginblock.dart
+++ lib/home/login/loginblock.dart
@@ -1,7 +1,6 @@
 import "package:flutter/material.dart";
 import "package:font_awesome_flutter/font_awesome_flutter.dart";
 import "package:provider/provider.dart";
-import "../../services/auth.dart";
 import "../../services/services.dart";
 
 class LoginBlock extends StatefulWidget {
