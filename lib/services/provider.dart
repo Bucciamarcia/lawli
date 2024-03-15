6b8a465b065788870e86a758d7999cc862fc4ceb
Stefano Mini <info@stefanominiconsulting.com>
2024-03-15 06:40:31 +0200

    default provider

--- lib/services/provider.dart
+++ lib/services/provider.dart
@@ -3,29 +3,29 @@ import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:lawli/services/models.dart';
 
 class DashboardProvider extends ChangeNotifier {
-
   double idPratica = kDebugMode ? 1 : 0;
-  String idDocument = "0";
-  Pratica pratica = Pratica();
-  Documento documento = Documento(data: DateTime.now());
+  Pratica pratica = kDebugMode
+      ? Pratica(id: 1, assistitoId: 2, titolo: "Pratica Mario Rossi")
+      : Pratica();
+  Documento documento = kDebugMode
+      ? Documento(
+          filename: "Gmail - New Order Confirmation_ IN139829.pdf",
+          brief_description: "Conferma ordine Analogue Seduction",
+          data: DateTime.now())
+      : Documento(data: DateTime.now());
 
   void setIdPratica(double id) {
     idPratica = id;
     notifyListeners();
   }
 
-  void setPratica (Pratica p) {
+  void setPratica(Pratica p) {
     pratica = p;
     notifyListeners();
   }
 
-  void setDocumento (Documento d) {
+  void setDocumento(Documento d) {
     documento = d;
-    notifyListeners();
-  }
-
-  void setIdDocument(String idDoc) {
-    idDocument = idDoc;
     notifyListeners();
   }
 }
