8c510313f5a76d13a7d57d8e32b77ab9653b2ddc
Stefano Mini <info@stefanominiconsulting.com>
2024-03-20 17:28:39 +0200

    create assitant ok

--- functions/py/functions/create_assistant.py
+++ functions/py/functions/create_assistant.py
@@ -10,11 +10,16 @@ class Create_Assistant:
         self.client = OpenAI(api_key=os.environ.get("OPENAI_APIKEY"))
         self.engine = CREATE_ASSISTANT_ENGINE
     
-    def upload_document(self, blob_name:str):
-        filebytes = Cloud_Storege_Util(logger).get_file_bytes("accounts/lawli/pratiche/1/documenti/originale_Gmail - New Order Confirmation_ IN139829.pdf")
+    def upload_document(self, blob_name:str) -> str:
         try:
-            self.client.files.create(filebytes, purpose="assistants")
+            filebytes = Cloud_Storege_Util(logger).get_file_bytes(blob_name)
+        except Exception as e:
+            logger.error(f"Error while reading file: {e}")
+            raise f"Error while reading file: {e}"
+        try:
+            response = self.client.files.create(file=filebytes, purpose="assistants")
             logger.info("Document uploaded successfully")
+            return response.id
         except Exception as e:
             logger.error(f"Error while uploading document: {e}")
             raise f"Error while uploading document: {e}"
@@ -23,9 +28,9 @@ class Create_Assistant:
         """
         Entrypoint. Creates the assistant with the given name.
         """
-        
+        document_id = self.upload_document("accounts/lawli/pratiche/1/documenti/originale_Gmail - New Order Confirmation_ IN139829.pdf")
         try:
-            my_assistant = self.client.beta.assistants.create(model=self.engine, name=assistant_name)
+            my_assistant = self.client.beta.assistants.create(model=self.engine, name=assistant_name, file_ids=[document_id], tools=[{"type": "retrieval"}])
             logger.info(f"MY_ASSISTANT: {my_assistant}")
             return my_assistant.id
         except Exception as e:
