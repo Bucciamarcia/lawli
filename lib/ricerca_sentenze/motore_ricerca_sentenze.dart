2ae1854c892393a54c1000dbb7d0f397e48c454d
Stefano Mini <info@stefanominiconsulting.com>
2024-04-06 17:44:45 +0300

    sentenze testo ok

--- lib/ricerca_sentenze/motore_ricerca_sentenze.dart
+++ lib/ricerca_sentenze/motore_ricerca_sentenze.dart
@@ -1,7 +1,8 @@
 import 'package:cloud_functions/cloud_functions.dart';
+import 'package:flutter/services.dart';
+import "dart:convert";
 import "sentenza_object.dart";
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:lawli/services/provider.dart';
 import 'package:provider/provider.dart';
 
@@ -31,13 +32,13 @@ class _MotoreRicercaSentenzeState extends State<MotoreRicercaSentenze>
           TabBar(
             controller: _tabController,
             tabs: const [
-              Tab(text: "Parole chiave"),
+              Tab(text: "Testo"),
               Tab(text: "Documento"),
               Tab(text: "Pratica"),
             ],
           ),
           SizedBox(
-            height: 1200,
+            height: 1300,
             child: TabBarView(
               controller: _tabController,
               children: const [
@@ -103,20 +104,34 @@ class TribunaleSelector extends StatelessWidget {
   }
 }
 
-Future<String?> textSendButtonPressed(String text, String corte) async {
-  var result = await FirebaseFunctions.instance.httpsCallable("get_similar_sentences").call(
+Future<List<Sentenza>> textSendButtonPressed(String text, String corte) async {
+  List<Sentenza> sentenze = [];
+  var result = await FirebaseFunctions.instance
+      .httpsCallable("get_similar_sentences")
+      .call(
     {
       "text": text,
       "corte": corte,
     },
   );
-  String resultData = result.data;
-  return resultData;
+  var data = result.data;
+  List<dynamic> dataList = jsonDecode(data);
+  for (dynamic o in dataList) {
+    sentenze.add(
+      Sentenza(
+        contenuto: o["contenuto"],
+        corte: o["corte"],
+        titolo: o["titolo"],
+        distance: o["distance"],
+      ),
+    );
+  }
+  return sentenze;
 }
 
 class _RicercaParoleChiaveState extends State<RicercaParoleChiave> {
   final _textController = TextEditingController();
-  String _resultText = ''; // Store the result string
+  List<Sentenza> similarSentenze = [];
 
   @override
   Widget build(BuildContext context) {
@@ -139,13 +154,10 @@ class _RicercaParoleChiaveState extends State<RicercaParoleChiave> {
                 },
               ),
             ),
-            submitIconButton(_textController.text,
-                clearController:
-                    true),
+            submitIconButton(_textController.text, clearController: true),
           ],
         ),
-        if (_resultText.isNotEmpty)
-          ResultBox(resultText: _resultText),
+        if (similarSentenze.isNotEmpty) ResultBox(sentenze: similarSentenze),
       ],
     );
   }
@@ -161,10 +173,13 @@ class _RicercaParoleChiaveState extends State<RicercaParoleChiave> {
       onPressed: textToCompare.isNotEmpty
           ? () async {
               // Only send if the input isn't empty
-              final result = await textSendButtonPressed(textToCompare, Provider.of<RicercaSentenzeProvider>(context, listen: false).corte);
+              List<Sentenza> result = await textSendButtonPressed(
+                  textToCompare,
+                  Provider.of<RicercaSentenzeProvider>(context, listen: false)
+                      .corte);
               if (clearController == true) {
                 setState(() {
-                  _resultText = result!;
+                  similarSentenze = result;
                   _textController.clear(); // Clear input after sending
                 });
               }
@@ -181,32 +196,102 @@ class _RicercaParoleChiaveState extends State<RicercaParoleChiave> {
 }
 
 class ResultBox extends StatelessWidget {
-  const ResultBox({
-    super.key,
-    required String resultText,
-  }) : _resultText = resultText;
+  final List<Sentenza> sentenze;
 
-  final String _resultText;
+  const ResultBox({super.key, required this.sentenze});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 10),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        border: Border.all(),
-      ),
-      child: Row(
-        children: [
-          Expanded(child: SelectableText(_resultText)), // Make text selectable
-          IconButton(
-            icon: const Icon(Icons.copy),
-            onPressed: () {
-              Clipboard.setData(ClipboardData(text: _resultText));
-            },
-          ),
-        ],
-      ),
-    );
+        margin: const EdgeInsets.only(top: 10),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(),
+        ),
+        child: ListView.builder(
+          itemCount: sentenze.length,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            final Sentenza sentenza = sentenze[index];
+            return Card(
+              elevation: 5,
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        sentenza.titolo,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(sentenza.corte, textAlign: TextAlign.end),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                          "Punteggio di similarità: ${similarityPercentage(sentenza.distance)}%"),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 10),
+                  IconButton(
+                    icon: const Icon(Icons.open_in_new_rounded),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text(sentenza.titolo),
+                            content: SingleChildScrollView(
+                              child: SelectableText(sentenza.contenuto),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text("Chiudi"),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Clipboard.setData(
+                                      ClipboardData(text: sentenza.contenuto));
+                                  showDialog(context: context, builder: (context) {
+                                    return AlertDialog(
+                                      title: const Text("Testo copiato"),
+                                      content: const Text("Il testo è stato copiato negli appunti."),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: const Text("Chiudi"),
+                                        ),
+                                      ],
+                                    );
+                                  });
+                                },
+                                child: const Text("Copia"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  )
+                ],
+              ),
+            );
+          },
+        ));
+  }
+
+  // Returns a % score of similarity, approximated to 2 decimal places
+  double similarityPercentage(double distance) {
+    double score = 1 - distance;
+    return (score * 10000).roundToDouble() / 100;
   }
 }
