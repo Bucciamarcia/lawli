a6aaf2d03eceff9f1652748c8949f26f4f71057c
Stefano Mini <info@stefanominiconsulting.com>
2024-04-24 20:33:45 +0300

    init pagina

---
+++ lib/trascrizione_audio/trascrizione_audio.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TrascrizioneAudioScreen extends StatelessWidget {
+  const TrascrizioneAudioScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
