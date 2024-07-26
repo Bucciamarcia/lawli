393e76f8723aacaaac1a8e8da3e92adf2cad74cb
Stefano Mini <info@stefanominiconsulting.com>
2024-07-26 10:25:42 +0300

    chore: Add ChangeNotifierProvider for TemplateProvider in TemplateHomeWidget

---
+++ lib/template/provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class TemplateProvider extends ChangeNotifier {
+
+}
--- lib/template/template_main.dart
+++ lib/template/template_main.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:lawli/services/firestore.dart';
+import 'package:provider/provider.dart';
+import 'provider.dart';
 
 class TemplateHomeWidget extends StatelessWidget {
   const TemplateHomeWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ChangeNotifierProvider(
+      create: (context) => TemplateProvider(),
+      child: Column(
+        children: [
+          const SizedBox(
+            width: 400,
+            height: 250,
+            child: Placeholder(),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FutureBuilder(
+                future: RetrieveObjectFromDb().getPratiche(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return const Text("Errore");
+                  } else {
+                    return Text("Pratiche: ${snapshot.data}");
+                  }
+                },
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                label: const Text("Nuovo modello"),
+                icon: const Icon(Icons.add),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
