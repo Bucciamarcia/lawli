5007c28a796ea35e7a5355fb9d0b0bedd95e8ee1
Stefano Mini <info@stefanominiconsulting.com>
2024-08-14 15:04:06 +0300

    Upgrade depend

--- lib/template/editor/pdf_export.dart
+++ lib/template/editor/pdf_export.dart
@@ -1,12 +1,23 @@
-import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class PdfExporter {
   final String templateText;
+  final pdf = pw.Document();
 
   PdfExporter({required this.templateText});
 
-  /// Exports the filled template to a PDF file
+  /// Entrypoint. Exports the filled template to a PDF file.
   Future<void> export() async {
-    debugPrint(templateText);
+    pdf.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Text(templateText),
+          );
+        },
+      ),
+    );
   }
 }
