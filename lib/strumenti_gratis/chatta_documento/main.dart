17f41d2ad12054ccb230f63b3684d44eaa66e361
Stefano Mini <info@stefanominiconsulting.com>
2024-10-04 10:39:38 +0300

    Caricamento documento chat standalone

--- lib/strumenti_gratis/chatta_documento/main.dart
+++ lib/strumenti_gratis/chatta_documento/main.dart
@@ -1,15 +1,41 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'models.dart';
+import 'package:lawli/shared/upload_file.dart';
+import 'package:lawli/strumenti_gratis/chatta_documento/provider.dart';
+import 'package:provider/provider.dart';
 
 class ChatMainStandalone extends StatelessWidget {
   const ChatMainStandalone({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("hello there!"),
-      ],
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 800),
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              FileUploader(
+                labelText: "Carica un documento per chattare",
+                helperText: "Carica un documento per chattare",
+                buttonText: "Carica",
+                onFileSelected: (PlatformFile? file) {
+                  Provider.of<DocumentChatterProvider>(context, listen: false)
+                      .updateFile(file);
+                },
+              ),
+              if (Provider.of<DocumentChatterProvider>(context, listen: true)
+                      .file !=
+                  null)
+                Text(Provider.of<DocumentChatterProvider>(context, listen: true)
+                    .file!
+                    .name),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/strumenti_gratis/chatta_documento/provider.dart
+++ lib/strumenti_gratis/chatta_documento/provider.dart
@@ -1,6 +1,18 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'models.dart';
 
 class DocumentChatterProvider extends ChangeNotifier {
-    List<ChatMessage> messaggi = [];
+  List<ChatMessage> messaggi = [];
+  PlatformFile? file;
+
+  void addChatMessage(ChatMessage message) {
+    messaggi.add(message);
+    notifyListeners();
   }
+
+  void updateFile(PlatformFile? file) {
+    this.file = file;
+    notifyListeners();
+  }
+}
